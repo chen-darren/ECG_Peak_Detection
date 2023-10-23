6099361996712a7e6459f509023db93584a00b54
Darren Chen <101908191+chen-darren@users.noreply.github.com>
2023-10-22 23:58:32 -0400

    Completed up through squaring and plotted each step

--- ecg_filtering.m
+++ ecg_filtering.m
@@ -7,14 +7,22 @@ ecg_noise_path = 'C:\Users\dchen\OneDrive - University of Connecticut\Courses\Ye
 % Load data - Darren: original data file converted to .csv
 ecg_noise_filename = 'ECG_with_noise.csv';
 ecg_noise = readmatrix(strcat(ecg_noise_path, filesep ,ecg_noise_filename));
-% figure; plot(ecg_noise);
 
 % Standardize data - Darren: ECG data is centered with STD ~ 1
 mean_ecg_noise = mean(ecg_noise);
 centered_ecg_noise = ecg_noise - mean_ecg_noise;
 std_dev_centered = std(centered_ecg_noise);
 standardized_ecg_noise = centered_ecg_noise / std_dev_centered;
-% figure; plot(standardized_ecg_noise);
+
+% Plot original and standardized
+figure;
+subplot(2,1,1);
+plot(ecg_noise);
+title('ECG with Noise');
+
+subplot(2,1,2);
+plot(standardized_ecg_noise);
+title('Standardized ECG with Noise');
 
 % Create the low-pass transfer function
 num_low_pass = [1 0 0 0 0 -2 0 0 0 0 1];  % [1-z^(-5)]^2 = 1 - 2z^(-5) + z^(-10))
@@ -31,24 +39,24 @@ low_pass_ecg = filter(num_low_pass, den_low_pass, standardized_ecg_noise);
 high_pass_ecg = filter(num_high_pass, den_high_pass, standardized_ecg_noise);
 bandpass_ecg = filter(num_high_pass, den_high_pass, low_pass_ecg);
 
-% % Plot original input, low-pass, high-pass, and bandpass
-% figure;
-% subplot(2,1,1);
-% plot(standardized_ecg_noise);
-% title('Standardized ECG with Noise');
-% 
-% subplot(2,1,2);
-% plot(bandpass_ecg);
-% title('Bandpass ECG');
-% 
-% figure;
-% subplot(2,1,1);
-% plot(low_pass_ecg);
-% title('Low-Pass ECG');
-% 
-% subplot(2,1,2);
-% plot(high_pass_ecg);
-% title('High-Pass ECG');
+% Plot original input, low-pass, high-pass, and bandpass
+figure;
+subplot(2,1,1);
+plot(standardized_ecg_noise);
+title('Standardized ECG with Noise');
+
+subplot(2,1,2);
+plot(bandpass_ecg);
+title('Bandpass ECG');
+
+figure;
+subplot(2,1,1);
+plot(low_pass_ecg);
+title('Low-Pass ECG');
+
+subplot(2,1,2);
+plot(high_pass_ecg);
+title('High-Pass ECG');
 
 % Create derivative transfer function
 num_der = [2 1 0 -1 -2];
@@ -61,13 +69,17 @@ derivative_ecg = filter(num_der, den_der, bandpass_ecg);
 % Square the derivative output
 squared_ecg = derivative_ecg.^2;
 
-% Plot bandpass and derivative
+% Plot bandpass, derivative, and squared
 figure;
 subplot(2,1,1);
 plot(bandpass_ecg);
 title('Bandpass ECG');
 
 subplot(2,1,2);
+plot(derivative_ecg);
+title('Derivative ECG');
+
+figure;
 plot(squared_ecg);
 title('Squared ECG');
 
