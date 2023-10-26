3f7b58a85575f31e7805c2807589bce997ab56ed
Darren Chen <101908191+chen-darren@users.noreply.github.com>
2023-10-26 18:38:53 -0400

    Changed everything to difference equations, up through squaring

--- ecg_filtering.m
+++ ecg_filtering.m
@@ -32,34 +32,33 @@ title('ECG with Noise');
 % Low-pass filter with difference equation
 low_pass_ecg = [ecg_noise(1) 2*ecg_noise(1)+ecg_noise(2)]; % Darren: y(1) = x(1), y(2) = 2y(1)+x(2), T = 1
 for n = 3:5 % Darren
-    y = 2*low_pass_ecg(n-1) - low_pass_ecg(n-2) + ecg_noise(n); % Darren: y(n) = 2(n-1) - y(n-2) + x(n)
-    low_pass_ecg = [low_pass_ecg y];
+    low_pass_ecg(n) = 2*low_pass_ecg(n-1) - low_pass_ecg(n-2) + ecg_noise(n); % Darren: y(n) = 2(n-1) - y(n-2) + x(n)
 end
+
 for n = 6:10
-    y = 2*low_pass_ecg(n-1) - low_pass_ecg(n-2) + ecg_noise(n) - 2*ecg_noise(n-5); % Darren: y(n) = 2(n-1) - y(n-2) + x(n) - 2x(n-5)
-    low_pass_ecg = [low_pass_ecg y];
+    low_pass_ecg(n) = 2*low_pass_ecg(n-1) - low_pass_ecg(n-2) + ecg_noise(n) - 2*ecg_noise(n-5); % Darren: y(n) = 2(n-1) - y(n-2) + x(n) - 2x(n-5)
 end
+
 for n = 11:size(ecg_noise,1)
-    y = 2*low_pass_ecg(n-1) - low_pass_ecg(n-2) + ecg_noise(n) - 2*ecg_noise(n-5) + ecg_noise(n-10); % Darren: y(n) = 2(n-1) - y(n-2) + x(n) - 2x(n-5) + x(n-10)
-    low_pass_ecg = [low_pass_ecg y];
+    low_pass_ecg(n) = 2*low_pass_ecg(n-1) - low_pass_ecg(n-2) + ecg_noise(n) - 2*ecg_noise(n-5) + ecg_noise(n-10); % Darren: y(n) = 2(n-1) - y(n-2) + x(n) - 2x(n-5) + x(n-10)
 end
 low_pass_ecg = low_pass_ecg.'; % Transpose into column vector
 
 % High-pass filter with difference equation
 bandpass_ecg = [(-1/32)*low_pass_ecg(1)];
+
 for n = 2:16
-    y = bandpass_ecg(n-1) - (1/32)*low_pass_ecg(n);
-    bandpass_ecg = [bandpass_ecg y];
+    bandpass_ecg(n) = bandpass_ecg(n-1) - (1/32)*low_pass_ecg(n);
 end
-y = bandpass_ecg(17-1) - (1/32)*low_pass_ecg(17) + low_pass_ecg(17-16);
-bandpass_ecg = [bandpass_ecg y];
+
+bandpass_ecg(17) = bandpass_ecg(17-1) - (1/32)*low_pass_ecg(17) + low_pass_ecg(17-16);
+
 for n = 18:32
-    y = bandpass_ecg(n-1) - (1/32)*low_pass_ecg(n) + low_pass_ecg(n-16) - low_pass_ecg(n-17);
-    bandpass_ecg = [bandpass_ecg y];
+    bandpass_ecg(n) = bandpass_ecg(n-1) - (1/32)*low_pass_ecg(n) + low_pass_ecg(n-16) - low_pass_ecg(n-17);
 end
+
 for n = 33:size(low_pass_ecg,1)
-    y = bandpass_ecg(n-1) - (1/32)*low_pass_ecg(n) + low_pass_ecg(n-16) - low_pass_ecg(n-17) + (1/32)*low_pass_ecg(n-32);
-    bandpass_ecg = [bandpass_ecg y]; 
+    bandpass_ecg(n) = bandpass_ecg(n-1) - (1/32)*low_pass_ecg(n) + low_pass_ecg(n-16) - low_pass_ecg(n-17) + (1/32)*low_pass_ecg(n-32);
 end
 bandpass_ecg = bandpass_ecg.';
 
@@ -70,16 +69,48 @@ plot(ecg_noise);
 title('ECG with Noise');
 
 subplot(2,1,2);
-plot(standardized_ecg_noise);
-title('Standardized ECG with Noise');
+plot(low_pass_ecg);
+title('Low-Pass ECG');
+
+figure;
+subplot(2,1,1);
+plot(ecg_noise);
+title('ECG with Noise');
+
+subplot(2,1,2);
+plot(bandpass_ecg);
+title('Bandpass ECG');
+
+figure;
+subplot(2,1,1);
+plot(low_pass_ecg);
+title('Low-Pass ECG');
+
+subplot(2,1,2);
+plot(bandpass_ecg);
+title('Bandpass ECG');
 
 % Create derivative transfer function
 num_der = [2 1 0 -1 -2];
-den_der = [0.1];
-H_z_derivative = tf(num_der, den_der, 1); % Creates transfer function for derivative function
+den_der = [1];
+H_z_derivative = tf(num_der, den_der, 0.125); % Creates transfer function for derivative function
 
 % Apply derivative transfer function to bandpass output
 derivative_ecg = filter(num_der, den_der, bandpass_ecg); % Applies previously created transfer function
+
+% Derivative difference function
+derivative_ecg = [2*bandpass_ecg(1)/8]; % y(1) = 2x(n)/8
+
+for n = 2:3
+    derivative_ecg(n) = (2*bandpass_ecg(n) + bandpass_ecg(n-1))/8; % y(n) = [2x(n) + x(n-1)]/8
+end
+
+derivative_ecg(4) = (2*bandpass_ecg(4) + bandpass_ecg(4-1) - bandpass_ecg(4-3))/8; % y(n) = [2x(n) + x(n-1) - x(n-3)]/8
+
+for n = 5:size(bandpass_ecg,1)
+    derivative_ecg(n) = (2*bandpass_ecg(n) + bandpass_ecg(n-1) - bandpass_ecg(n-3) - 2*bandpass_ecg(n-4))/8; % y(n) = [2x(n) + x(n-1) - x(n-3) - 2x(n-4)]/8
+end
+derivative_ecg = derivative_ecg.';
 
 % Square the derivative output
 squared_ecg = derivative_ecg.^2;
